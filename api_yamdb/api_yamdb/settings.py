ce744849ff43bab9021be2c66cc72d35c8b201d0
Nikolay Nikitenko <nikitenkonik@mail.ru>
2023-04-10 11:34:29 +0300

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,12 +5,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', default=None)
+SECRET_KEY = os.getenv('SECRET_KEY', default='X')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=None)
+DEBUG = os.getenv('DEBUG', default=True)
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default=None)]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='x')]
 
 
 # Application definition
